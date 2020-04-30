cb965954b909f0c963750b553081d47262b5ef34
Tejas <56431505+tejasvtanna@users.noreply.github.com>
2020-04-30 16:27:55 +0530

    Updates

---
+++ precache-manifest.706bec19d0dc6cf008d8ac4268827e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd17dbcb56d92f0e108f2c9a492feb2",
+    "url": "/currency-coverter/index.html"
+  },
+  {
+    "revision": "986cdad3ee87d3b886c5",
+    "url": "/currency-coverter/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d03fc8c0f8f89dbd83b1",
+    "url": "/currency-coverter/static/css/main.affdd800.chunk.css"
+  },
+  {
+    "revision": "986cdad3ee87d3b886c5",
+    "url": "/currency-coverter/static/js/2.5a5de156.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/currency-coverter/static/js/2.5a5de156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03fc8c0f8f89dbd83b1",
+    "url": "/currency-coverter/static/js/main.bc967f52.chunk.js"
+  },
+  {
+    "revision": "0ded741f62de1163d1b3",
+    "url": "/currency-coverter/static/js/runtime-main.8be1b8d6.js"
+  }
+]);
